4963586c209a6564e0b8e8e3df1ad906c3de9af0
Gohan1233 <gar21352@uvg.edu.gt>
2021-11-09 17:50:57 -0600

    Agregar productos parte8
    Agregamos relojes

---
+++ Smartwatch.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Date;
+
+/**
+ * Clase para los smartwatch
+ * 
+ * @author Gabriel Garcia
+ * @version 07/11/2021
+ */
+
+public class Smartwatch extends Producto implements Llamadas, Fotos, Portable{
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param precio el precio en Q
+	 * @param serie la serie del producto
+	 * @param marca la marca del producto
+	 * @param fecha la fecha en formato Date
+	 */
+	public Smartwatch(int precio, String serie, String marca, Date fecha){
+		super(precio, serie, marca, fecha);
+		articulo = "Smartwatch";
+		tags.add("Toma fotos");
+		tags.add("Hace llamadas");
+		tags.add("Portable");  
+	}
+
+	/**
+	 * Determina la accion en realizar en base a la caracteristica seleccionada
+	 * 
+	 * @param cond la caracteristica a demostrar
+	 * @return un string describiendo la accion
+	 */
+	public String accion(String cond){
+		if(cond.equals("Hace llamadas")){
+			return hacerLlamada();
+		}
+		if(cond.equals("Toma fotos")){
+			return tomarFoto();
+		}
+		if(cond.equals("Portable")){
+			return serPortable();
+		}
+		return " ";
+	}
+
+	@Override
+	public String hacerLlamada(){
+		System.out.println("Ingrese el numero a llamar: ");
+		return "Llamando al numero " + new Scanner(System.in).nextLine() + " desde mi " + getArticulo() + " "  + getMarca();
+	}
+
+	@Override
+	public String tomarFoto(){
+		return "Tomando foto con la camara de mi " + getArticulo() + " "  + getMarca();
+	}
+	
+	@Override
+	public String serPortable(){
+		return "Poniendo mi " + getArticulo() + " "  + getMarca() + " en mi muneca...";
+	}
+
+}
